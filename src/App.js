c9942c51dcec79fc4d24aad10d43b729f073e534
SarahHessel <sarah.phessel@gmail.com>
2020-12-07 17:34:26 -0300

    app limpo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import CartPage from './screens/CartPage'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <CartPage/>
+      
     </div>
   );
 }
