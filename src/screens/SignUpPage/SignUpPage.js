f383b7c9345c3563879b19565ee641683d9e55bf
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-12-08 19:52:29 -0300

    feito o hooks useForm e logica do signup page

--- src/screens/SignUpPage/SignUpPage.js
+++ src/screens/SignUpPage/SignUpPage.js
@@ -4,8 +4,28 @@ import { Input } from "../../components/Input/Input";
 import { Button } from "../../components/Button/Button";
 import { LoginPageContainer } from "../LoginPage/styled-LoginPage";
 import { Header } from "../../components/Header/Header";
+import { useHistory } from "react-router-dom"
+import { useEffect } from "react";
+import { baseUrl } from "../../constants/baseUrl"
+import { useForm } from "../../hooks/useForm";
+
 export function SignUpPage() {
   // Página de SignUp do usuário
+
+const history = useHistory()
+const { form, onChange } = useForm({name: "", email:"", cpf:"", password:""})
+
+const handleInputChange = (event) => {
+  const { value, name } = event.target
+
+  onChange(value, name)
+}
+
+const handleSubmission = (event) => {
+  event.preventDefault()
+  login(form, history)
+}
+
   return (
     <div>
       <Header />
