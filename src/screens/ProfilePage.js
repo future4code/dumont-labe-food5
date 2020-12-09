b47cd382c40951b107aae9104c9f11f2ac151a32
Mateus <mateus2050@gmail.com>
2020-12-09 11:37:49 -0200

    componente card restaurante

--- src/screens/ProfilePage.js
+++ src/screens/ProfilePage.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { RestaurantCard } from "../components/RestaurantCard/RestaurantCard";
 
 function ProfilePage() {
   return (
-    <div>
-      Meu perfil
-      <hr />
-      <div>Editar informações pessoais (ProfileEditUserPage)</div>
-      <div>Endereço cadastrado (ProfilePageEditAdress)</div>
-      <div>
-        Histórico de pedidos
-        <hr />
-        <div>Você não realizou nenhum pedido</div>
-      </div>
-    </div>
+    <RestaurantCard/>
+    // <div>
+    //   Meu perfil
+    //   <hr />
+    //   <div>Editar informações pessoais (ProfileEditUserPage)</div>
+    //   <div>Endereço cadastrado (ProfilePageEditAdress)</div>
+    //   <div>
+    //     Histórico de pedidos
+    //     <hr />
+    //     <div>Você não realizou nenhum pedido</div>
+    //   </div>
+    // </div>
   );
 }
 
