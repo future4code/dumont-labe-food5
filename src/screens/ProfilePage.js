99f7fc885814b2635a3bf2b390b65463b647fb8a
Mateus <mateus2050@gmail.com>
2020-12-09 11:50:00 -0200

    implementação card na página do feed

--- src/screens/ProfilePage.js
+++ src/screens/ProfilePage.js
@@ -3,18 +3,18 @@ import { RestaurantCard } from "../components/RestaurantCard/RestaurantCard";
 
 function ProfilePage() {
   return (
-    <RestaurantCard/>
-    // <div>
-    //   Meu perfil
-    //   <hr />
-    //   <div>Editar informações pessoais (ProfileEditUserPage)</div>
-    //   <div>Endereço cadastrado (ProfilePageEditAdress)</div>
-    //   <div>
-    //     Histórico de pedidos
-    //     <hr />
-    //     <div>Você não realizou nenhum pedido</div>
-    //   </div>
-    // </div>
+    
+    <div>
+      Meu perfil
+      <hr />
+      <div>Editar informações pessoais (ProfileEditUserPage)</div>
+      <div>Endereço cadastrado (ProfilePageEditAdress)</div>
+      <div>
+        Histórico de pedidos
+        <hr />
+        <div>Você não realizou nenhum pedido</div>
+      </div>
+    </div>
   );
 }
 
--- src/screens/feedPage/feedPage.js
+++ src/screens/feedPage/feedPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { PageContainer, InputStyle, FilterContainer, CardImg, CardContainer, CardDetail, BottomBar} from "./styled";
 import { baseUrl } from '../../constants/baseUrl'
 import { useRequestData } from '../../hooks/useRequestData'
+import { RestaurantCard } from "../../components/RestaurantCard/RestaurantCard";
 
 
 
@@ -22,14 +23,7 @@ function FeedPage() {
         <p>Massas</p>
         <p>Saudáveis</p>
       </FilterContainer>
-      <CardContainer>
-        <CardImg src="https://cdn.zeplin.io/5dd5ab8e5fb2a0060f81698f/assets/C78C3992-5FF3-4F52-8632-E1CA759C9C9F.png" />
-        <p>Vinil Butantã</p>
-        <CardDetail>
-          <p>50 - 60 min</p>
-          <p>Frete R$ 6,00</p>
-        </CardDetail>
-      </CardContainer>
+      <RestaurantCard/>
       <BottomBar>
         <img src="https://cdn.zeplin.io/5dd5ab8e5fb2a0060f81698f/assets/E718CCC7-08DF-4BEA-B3D1-8DCB3E8A3BA5.svg" />
         <img src="https://cdn.zeplin.io/5dd5ab8e5fb2a0060f81698f/assets/31E0BDE3-26B3-421A-AEC5-883D098413D6.svg" />
