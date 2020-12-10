86a740de91afe0b3c586dcceef0ce795dff974b4
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-12-10 20:47:10 -0300

    pasta para style e edit profile

--- src/screens/ProfileEditUserPage/ProfileEditUserPage.js
+++ src/screens/ProfileEditUserPage/ProfileEditUserPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useProtectPage } from "../hooks/useProtectPage";
+import { useProtectPage } from "../../hooks/useProtectPage";
 
 function ProfileEditUserPage() {
   useProtectPage();
