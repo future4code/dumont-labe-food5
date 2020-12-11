fd829e3a2b74307e58ee5aede8ce72fb6bd193a3
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-12-11 18:24:08 -0300

    ajuste nos nomes do topo conforme rotas

--- src/components/Header/styled-Header.js
+++ src/components/Header/styled-Header.js
@@ -4,30 +4,32 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   display: flex;
-  width: 100vw;
+  width: 100%;
   margin-top: 0;
   padding: 0 3vw;
   height: 6vh;
   border-bottom: 1px solid black;
   justify-content: space-between;
   margin-bottom: 3vh;
-  align-items: center;
+  align-items: center; 
+p {
+  margin-right: 5vw;
+  :hover {
+    color: #e8222e;
+  }
+}
+`;
 
-  h1 {
-    font-family: Roboto;
-    font-size: 1rem;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: -0.39px;
-    text-align: center;
-    color: black;
-  }
-  p {
-    margin-right: 5vw;
-    :hover {
-      color: #e8222e;
-    }
-  }
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 0px;
+`;
+
+export const SmallContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 6vh;
 `;
